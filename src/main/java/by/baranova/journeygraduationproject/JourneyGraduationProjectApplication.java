418dd5a13f6573748053b18170e86edaa3028141
BaranovaKate <katte.bar19@gmail.com>
2024-04-27 12:29:10 +0300

    Creating a project

---
+++ src/main/java/by/baranova/journeygraduationproject/JourneyGraduationProjectApplication.java
@@ -0,0 +1,13 @@
+package by.baranova.journeygraduationproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JourneyGraduationProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JourneyGraduationProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/baranova/journeygraduationproject/JourneyGraduationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package by.baranova.journeygraduationproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JourneyGraduationProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
