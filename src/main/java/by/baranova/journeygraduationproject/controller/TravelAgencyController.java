75337b0b4df53ed358fa9712e9eb78e2e635d555
BaranovaKate <katte.bar19@gmail.com>
2024-04-27 14:31:11 +0300

    add methods to TravelAgencyController

--- src/main/java/by/baranova/journeygraduationproject/controller/TravelAgencyController.java
+++ src/main/java/by/baranova/journeygraduationproject/controller/TravelAgencyController.java
@@ -1,11 +1,40 @@
 package by.baranova.journeygraduationproject.controller;
 
+import by.baranova.journeygraduationproject.model.TravelAgency;
+import by.baranova.journeygraduationproject.service.AgencyService;
 import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-@Controller
+
+
+import java.util.List;
+
+@RestController
 @RequestMapping("/travel-agencies")
 @AllArgsConstructor
 public class TravelAgencyController {
+
+    private final AgencyService agencyService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TravelAgency> getTravelAgencyById(
+            final @PathVariable Long id) {
+        TravelAgency travelAgency = agencyService.findAgencyById(id);
+        return ResponseEntity.ok(travelAgency);
+
+    }
+
+    @GetMapping("/all")
+
+    public List<TravelAgency> getAllTravelAgenciesWithJourneys() {
+        return agencyService.findAgencies();
+    }
+
+    @PostMapping("/create")
+    public String createTravelAgency(
+            final @RequestBody TravelAgency newTravelAgency) {
+        agencyService.save(newTravelAgency);
+        return "Successfully created a new agency";
+    }
 
 }
