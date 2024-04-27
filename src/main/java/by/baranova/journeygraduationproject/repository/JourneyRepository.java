023540c43ada8222d7411d8ef6671a6e3b53a02c
BaranovaKate <katte.bar19@gmail.com>
2024-04-27 12:55:49 +0300

    add Repository

---
+++ src/main/java/by/baranova/journeygraduationproject/repository/JourneyRepository.java
@@ -0,0 +1,11 @@
+package by.baranova.journeygraduationproject.repository;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@AllArgsConstructor
+@Repository
+public class JourneyRepository {
+
+
+}
