71ce2f74da2c57b926564486565acf4ab15424dc
BaranovaKate <katte.bar19@gmail.com>
2024-04-27 12:48:13 +0300

    add fields for Journey and TravelAgency

--- src/main/java/by/baranova/journeygraduationproject/model/Journey.java
+++ src/main/java/by/baranova/journeygraduationproject/model/Journey.java
@@ -1,11 +1,47 @@
 package by.baranova.journeygraduationproject.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.Data;
+
+import java.time.LocalDate;
+
+/**
+ * Entity class representing a journey.
+ */
 
 @Data
 @Entity
 @Table(name = "journeys")
 public class Journey {
+    /** Constant. */
+    private static final int CONST = 32;
 
+    /** Unique identifier for the journey. */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    /** The country of the journey. */
+    @Column(name = "country", nullable = false, length = CONST)
+    private String country;
+
+    /** The town of the journey. */
+    @Column(name = "town", nullable = false, length = CONST)
+    private String town;
+
+    /** The end date of the journey. */
+    @Column(name = "dateToJourney", nullable = false)
+    private LocalDate dateToJourney;
+
+    /** The start date of the journey. */
+    @Column(name = "dateFromJourney", nullable = false)
+    private LocalDate dateFromJourney;
+
+    /** The travel agency associated with the journey. */
+    @ManyToOne
+    @JoinColumn(name = "travel_agency_id", nullable = false)
+    @JsonIgnoreProperties("journeys")
+    private TravelAgency travelAgency;
 }
--- src/main/java/by/baranova/journeygraduationproject/model/TravelAgency.java
+++ src/main/java/by/baranova/journeygraduationproject/model/TravelAgency.java
@@ -1,5 +1,6 @@
 package by.baranova.journeygraduationproject.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -9,5 +10,18 @@ import java.util.List;
 @Entity
 @Table(name = "travel_agencies")
 public class TravelAgency {
+    private static final int CONST = 64;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = CONST)
+    private String name;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "travelAgency",
+            cascade = CascadeType.ALL)
+    @JsonIgnoreProperties("travelAgency")
+    private List<Journey> journeys;
 }
