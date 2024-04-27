b4894b9419ef122cfcd7c8b0149b47899159c4ab
BaranovaKate <katte.bar19@gmail.com>
2024-04-27 14:27:29 +0300

    add methods to JourneyController

--- src/main/java/by/baranova/journeygraduationproject/controller/JourneyController.java
+++ src/main/java/by/baranova/journeygraduationproject/controller/JourneyController.java
@@ -1,13 +1,47 @@
 package by.baranova.journeygraduationproject.controller;
 
+import by.baranova.journeygraduationproject.dto.JourneyDto;
+import by.baranova.journeygraduationproject.service.JourneyService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/journeys")
-
+@AllArgsConstructor
 public class JourneyController {
 
+    private static final String ERROR = "404 Not Found: {}";
 
+    private final JourneyService journeyService;
+
+    @GetMapping
+    public List<JourneyDto> findJourneys() {
+        return journeyService.findJourneys();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<JourneyDto> findJourney(
+            final @PathVariable("id") Long id) {
+        JourneyDto journey = journeyService.findJourneyById(id);
+        return ResponseEntity.ok(journey);
+
+    }
+
+    @PostMapping("/new")
+    public String handleJourneyCreation(
+            final @Valid @RequestBody JourneyDto journey) {
+        journeyService.save(journey);
+        return "Successfully created a new journey";
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> handleJourneyDelete(final @PathVariable Long id) {
+        journeyService.deleteById(id);
+        return ResponseEntity
+                .ok("Successfully deleted journey with id " + id);
+    }
 }
