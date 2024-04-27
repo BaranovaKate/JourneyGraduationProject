23b986f051932adc8b784a5f8857c5b752766f24
BaranovaKate <katte.bar19@gmail.com>
2024-04-27 22:02:39 +0300

    add Swagger documentation for TravelAgencyController

--- src/main/java/by/baranova/journeygraduationproject/controller/TravelAgencyController.java
+++ src/main/java/by/baranova/journeygraduationproject/controller/TravelAgencyController.java
@@ -3,6 +3,8 @@ package by.baranova.journeygraduationproject.controller;
 import by.baranova.journeygraduationproject.dto.TravelAgencyDto;
 import by.baranova.journeygraduationproject.model.TravelAgency;
 import by.baranova.journeygraduationproject.service.AgencyService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import org.apache.logging.log4j.LogManager;
@@ -17,16 +19,25 @@ import java.util.List;
 @RestController
 @RequestMapping("/travel-agencies")
 @AllArgsConstructor
+@Tag(name = "Работа с туристическими агенствами",
+        description = "Данный контроллер позволяет получать,"
+                +  " добавлять, обновлять и удалять тур агенства")
 public class TravelAgencyController {
+    private static final String ERROR = "404 Not Found: {}";
 
     private final AgencyService agencyService;
-
-    private static final String ERROR = "404 Not Found: {}";
 
     static final Logger LOGGER = LogManager
             .getLogger(TravelAgencyController.class);
 
+
     @GetMapping("/{id}")
+    @Operation(
+            method = "GET",
+            summary = "Получить список тур агентств по id",
+            description = "Выводит тур агентство по id,"
+                    + " содержащеесся в базе данных"
+    )
     public ResponseEntity<TravelAgency> getTravelAgencyById(
             final @PathVariable Long id) {
         try {
@@ -40,12 +51,24 @@ public class TravelAgencyController {
     }
 
     @GetMapping("/all")
+    @Operation(
+            method = "GET",
+            summary = "Получить список всех тур агентств",
+            description = "Выводит список всех тур агентств,"
+                    + " содержащихся в базе данных"
+    )
     public List<TravelAgency> getAllTravelAgenciesWithJourneys() {
         LOGGER.info("Display Travel Agencies");
         return agencyService.findAgencies();
     }
 
     @PostMapping("/create")
+    @Operation(
+            method = "POST",
+            summary = "Создать туристическое агенство",
+            description = "Создает новое туристическе агенство"
+                    + " и добавляет его в базу данных."
+    )
     public String createTravelAgency(
             final @RequestBody TravelAgency newTravelAgency) {
         agencyService.save(newTravelAgency);
@@ -54,6 +77,11 @@ public class TravelAgencyController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(
+            method = "DELETE",
+            summary = "Удалить туристическое агенство",
+            description = "Удаляет туристическое агенство из базы данных"
+    )
     public ResponseEntity<String> deleteTravelAgencyById(
             final @PathVariable Long id) {
         try {
@@ -69,6 +97,12 @@ public class TravelAgencyController {
     }
 
     @PutMapping("/{id}")
+    @Operation(
+            method = "PUT",
+            summary = "Обновить туристическое агентство",
+            description = "Обновляет существуещее "
+                    + "туристичесоке агенство в базе данных,"
+    )
     public ResponseEntity<String> handleAgencyUpdate(
             final @PathVariable Long id,
             final @RequestBody TravelAgencyDto travelAgency) {
