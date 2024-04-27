c35814d2cfbd2ccb7f89a2dc07188400ed92d8d1
BaranovaKate <katte.bar19@gmail.com>
2024-04-27 21:38:48 +0300

    add delete and update for AgencyService

--- src/main/java/by/baranova/journeygraduationproject/service/AgencyService.java
+++ src/main/java/by/baranova/journeygraduationproject/service/AgencyService.java
@@ -1,7 +1,10 @@
 package by.baranova.journeygraduationproject.service;
-
+import by.baranova.journeygraduationproject.repository.JourneyRepository;
+import by.baranova.journeygraduationproject.dto.TravelAgencyDto;
+import by.baranova.journeygraduationproject.model.Journey;
 import by.baranova.journeygraduationproject.model.TravelAgency;
 import by.baranova.journeygraduationproject.repository.TravelAgencyRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +15,8 @@ import java.util.List;
 public class AgencyService {
 
     private final TravelAgencyRepository travelAgencyRepository;
+
+    private final JourneyRepository journeyRepository;
 
     public TravelAgency findAgencyById(final Long id) {
         return travelAgencyRepository.findById(id);
@@ -25,5 +30,20 @@ public class AgencyService {
         return travelAgencyRepository.findAllWithJourneys();
     }
 
+    public void deleteById(final Long id) {
+        List<Journey> journeysWithAgency = journeyRepository
+                .findByTravelAgencyId(id);
+        journeysWithAgency.forEach(journey -> journeyRepository
+                .deleteById(journey.getId()));
+        travelAgencyRepository.deleteById(id);
+    }
 
+    public void update(final Long id, final TravelAgencyDto updatedAgency) {
+        TravelAgency agency = findAgencyById(id);
+        if (agency == null) {
+            throw new EntityNotFoundException(
+                    "Travel agency with ID " + id + " does not exist");
+        }
+        travelAgencyRepository.update(id, updatedAgency);
+    }
 }
