4cc9a9bc1524cc2ea8ecd58f7ec8fee1e6d7b98b
BaranovaKate <katte.bar19@gmail.com>
2024-04-27 12:58:01 +0300

    add DTO for Journey

---
+++ src/main/java/by/baranova/journeygraduationproject/dto/JourneyDto.java
@@ -0,0 +1,34 @@
+package by.baranova.journeygraduationproject.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Data
+public class JourneyDto implements Serializable {
+
+    private static final int CONST = 20;
+
+    private Long id;
+
+    @NotBlank
+    @Size(min = 2, max = CONST)
+    private String country;
+
+    @NotBlank
+    @Size(min = 2, max = CONST)
+    private String town;
+
+    @NotNull
+    private LocalDate dateToJourney;
+
+    @NotNull
+    private LocalDate dateFromJourney;
+
+    private TravelAgencyDto travelAgency;
+
+}
