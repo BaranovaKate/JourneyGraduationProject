4adf098b11ccfb650f2aeb38e4e6de1d7015c001
BaranovaKate <katte.bar19@gmail.com>
2024-04-27 12:46:53 +0300

    add controller and model

---
+++ src/main/java/by/baranova/journeygraduationproject/controller/TravelAgencyController.java
@@ -0,0 +1,11 @@
+package by.baranova.journeygraduationproject.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+@Controller
+@RequestMapping("/travel-agencies")
+@AllArgsConstructor
+public class TravelAgencyController {
+
+}
---
+++ src/main/java/by/baranova/journeygraduationproject/model/Journey.java
@@ -0,0 +1,11 @@
+package by.baranova.journeygraduationproject.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "journeys")
+public class Journey {
+
+}
