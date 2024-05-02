ecc5c6f348262b21380a5561a07dc27cbad1d775
BaranovaKate <katte.bar19@gmail.com>
2024-05-02 10:45:53 +0300

    refactoring repository to use JPA

--- src/main/java/by/baranova/journeygraduationproject/repository/JourneyRepository.java
+++ src/main/java/by/baranova/journeygraduationproject/repository/JourneyRepository.java
@@ -1,104 +1,41 @@
 package by.baranova.journeygraduationproject.repository;
 
-import by.baranova.journeygraduationproject.dto.JourneyDto;
-import by.baranova.journeygraduationproject.mapper.JourneyMapper;
 import by.baranova.journeygraduationproject.model.Journey;
-import jakarta.persistence.EntityNotFoundException;
-import lombok.AllArgsConstructor;
-import org.hibernate.SessionFactory;
-import org.hibernate.query.MutationQuery;
-import org.hibernate.query.Query;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 
-@AllArgsConstructor
 @Repository
-public class JourneyRepository {
+public interface JourneyRepository extends JpaRepository<Journey, Long> {
 
-    private final SessionFactory sessionFactory;
-
-    private final JourneyMapper journeyMapper;
-
-    public List<JourneyDto> findAll() {
-        final List<Journey> journeys = sessionFactory.fromSession(session -> {
-            Query<Journey> query = session.createQuery(
-                    "FROM Journey j JOIN FETCH j.travelAgency ", Journey.class);
-            return query.list();
-        });
-        return journeys.stream()
-                .map(journeyMapper::toDto)
-                .toList();
-    }
-
-    public Optional<JourneyDto> findById(final Long id) {
-        return Optional.ofNullable(sessionFactory.fromSession(session -> {
-            Query<Journey> query = session.createQuery("""
-                FROM Journey J JOIN FETCH J.travelAgency\s
-                WHERE J.id = :id""", Journey.class);
-            query.setParameter("id", id);
-            return query.uniqueResult();
-        })).map(journeyMapper::toDto);
-    }
+    @org.springframework.data.jpa.repository.Query("SELECT j FROM Journey j WHERE j.travelAgency.id = :travelAgencyId")
+    List<Journey> findByTravelAgencyId(@Param("travelAgencyId") Long travelAgencyId);
 
 
-    public void deleteById(final Long id) {
-        Optional<JourneyDto> journeyOptional = findById(id);
-        if (journeyOptional.isEmpty()) {
-            throw new EntityNotFoundException(
-                    "Journey with id " + id + " not found");
-        }
-        sessionFactory.inTransaction(session -> {
-            final MutationQuery query = session.createMutationQuery("""
-                DELETE FROM Journey
-                WHERE id = :id
-                """);
-            query.setParameter("id", id);
-            query.executeUpdate();
-        });
-    }
+    @Modifying
+    @Transactional
+    @Query("""
+        UPDATE Journey J SET
+            J.country = :country,
+            J.town = :town,
+            J.dateToJourney = :dateToJourney,
+            J.dateFromJourney = :dateFromJourney,
+            J.travelAgency.id = :travel_agency_id
+        WHERE J.id = :id
+    """)
+    void updateJourney(
+            @Param("id") Long id,
+            @Param("country") String country,
+            @Param("town") String town,
+            @Param("dateToJourney") LocalDate dateToJourney,
+            @Param("dateFromJourney") LocalDate dateFromJourney,
+            @Param("travel_agency_id") Long travelAgencyId
+    );
 
-    public void save(final JourneyDto journeyDto) {
-        final Journey journey = journeyMapper.toEntity(journeyDto);
-        sessionFactory.inTransaction(session -> session.persist(journey));
-    }
-
-    public void update(final Long id, final JourneyDto journey) {
-        if (findById(id).isEmpty()) {
-            throw new EntityNotFoundException(
-                    "Journey with id " + id + " does not exist");
-        }
-        sessionFactory.inTransaction(session -> {
-            final MutationQuery query =
-                    session.createMutationQuery("""
-            UPDATE Journey J SET
-               J.country = :country,
-               J.town = :town,\s
-               J.dateToJourney = :dateToJourney,
-               J.dateFromJourney = :dateFromJourney,
-               J.travelAgency.id = :travel_agency_id
-            WHERE J.id = :id""");
-
-            query.setParameter("id", id);
-            query.setParameter("country", journey.getCountry());
-            query.setParameter("town", journey.getTown());
-            query.setParameter("dateToJourney", journey.getDateToJourney());
-            query.setParameter("dateFromJourney",
-                    journey.getDateFromJourney());
-            query.setParameter("travel_agency_id",
-                    journey.getTravelAgency().getId());
-            query.executeUpdate();
-        });
-    }
-
-    public List<Journey> findByTravelAgencyId(final Long travelAgencyId) {
-        return sessionFactory.fromSession(session -> {
-            Query<Journey> query = session.createQuery(
-                    "FROM Journey J WHERE J.travelAgency.id = "
-                            + ":travelAgencyId", Journey.class);
-            query.setParameter("travelAgencyId", travelAgencyId);
-            return query.list();
-        });
-    }
 }
--- src/main/java/by/baranova/journeygraduationproject/repository/TravelAgencyRepository.java
+++ src/main/java/by/baranova/journeygraduationproject/repository/TravelAgencyRepository.java
@@ -1,79 +1,20 @@
 package by.baranova.journeygraduationproject.repository;
 
-import by.baranova.journeygraduationproject.dto.TravelAgencyDto;
 import by.baranova.journeygraduationproject.model.TravelAgency;
-import jakarta.persistence.EntityNotFoundException;
-import lombok.AllArgsConstructor;
-import org.hibernate.SessionFactory;
-import org.hibernate.query.MutationQuery;
-import org.hibernate.query.Query;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@AllArgsConstructor
 @Repository
-public class TravelAgencyRepository {
+public interface TravelAgencyRepository extends JpaRepository<TravelAgency, Long> {
 
-    private final SessionFactory sessionFactory;
+    @org.springframework.data.jpa.repository.Query("SELECT ta FROM TravelAgency ta LEFT JOIN FETCH ta.journeys")
+    List<TravelAgency> findAllWithJourneys();
 
-    public TravelAgency findById(final Long id) {
-        return sessionFactory.fromSession(session -> {
-            Query<TravelAgency> query = session.createQuery("""
-                            FROM TravelAgency J LEFT JOIN FETCH J.journeys\s
-                            WHERE J.id = :id""",
-                    TravelAgency.class);
-            query.setParameter("id", id);
-            return query.uniqueResult();
-        });
-    }
-
-    public List<TravelAgency> findAllWithJourneys() {
-        return sessionFactory.fromSession(session -> {
-            Query<TravelAgency> query = session
-                    .createQuery(
-                            "FROM TravelAgency J LEFT JOIN FETCH J.journeys",
-                            TravelAgency.class);
-            return query.list();
-        });
-    }
-
-    public void save(final TravelAgency travelAgency) {
-        sessionFactory.inTransaction(session -> session.persist(travelAgency));
-    }
-
-    public void deleteById(final Long id) {
-        TravelAgency agencyToDelete = findById(id);
-        if (agencyToDelete == null) {
-            throw new EntityNotFoundException(
-                    "Агентство с id " + id + " не найдено");
-        }
-
-        sessionFactory.inTransaction(session -> {
-            final MutationQuery query = session
-                    .createMutationQuery(
-                            "DELETE FROM TravelAgency WHERE id = :id");
-            query.setParameter("id", id);
-            query.executeUpdate();
-        });
-    }
-
-    public void update(final Long id, final TravelAgencyDto updatedAgency) {
-        if (findById(id) == null) {
-            throw new EntityNotFoundException(
-                    "Агентство с id " + id + " не существует");
-        }
-        sessionFactory.inTransaction(session -> {
-            final MutationQuery query = session.createMutationQuery("""
-                    UPDATE TravelAgency T SET
-                       T.name = :name
-                    WHERE T.id = :id
-                    """);
-
-            query.setParameter("id", id);
-            query.setParameter("name", updatedAgency.getName());
-            query.executeUpdate();
-        });
-    }
+    @Query("SELECT t FROM TravelAgency t LEFT JOIN FETCH t.journeys WHERE t.id = :id")
+    TravelAgency findAById(@Param("id") Long id);
 
 }
