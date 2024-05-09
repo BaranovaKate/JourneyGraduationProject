1776bb8459ced52a0e0e9caf86bed8c52ae08d77
BaranovaKate <katte.bar19@gmail.com>
2024-05-09 22:49:02 +0300

    add some error handling

--- src/main/java/by/baranova/journeygraduationproject/controller/TravelerController.java
+++ src/main/java/by/baranova/journeygraduationproject/controller/TravelerController.java
@@ -1,64 +1,101 @@
 package by.baranova.journeygraduationproject.controller;
+
 import by.baranova.journeygraduationproject.model.Traveler;
 import by.baranova.journeygraduationproject.service.TravelerService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
 @RestController
 @RequestMapping("/travelers")
 @AllArgsConstructor
 public class TravelerController {
+    private static final Logger LOGGER = LogManager.getLogger(TravelAgencyController.class);
 
     private final TravelerService travelerService;
 
     @GetMapping
+    @Operation(
+            method = "GET",
+            summary = "Получить список всех туристических агентств",
+            description = "Возвращает список всех туристических агентств"
+    )
     public ResponseEntity<List<Traveler>> getAllTravelers() {
         List<Traveler> travelers = travelerService.getAllTravelers();
+        LOGGER.info("Retrieved all Travel Agencies");
         return ResponseEntity.ok(travelers);
     }
 
     @GetMapping("/{id}")
+    @Operation(
+            method = "GET",
+            summary = "Получить туристическое агентство по ID",
+            description = "Возвращает туристическое агентство по ID"
+    )
     public ResponseEntity<Traveler> getTravelerById(@PathVariable Long id) {
         try {
+            LOGGER.info("Fetching Travel Agency with ID: {}", id);
             Traveler traveler = travelerService.findTravelerById(id);
             return ResponseEntity.ok(traveler);
         } catch (EntityNotFoundException e) {
+            LOGGER.error("Travel Agency not found with ID: {}", id);
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         }
     }
 
     @PostMapping
-    public ResponseEntity<Traveler> addTraveler(
+    @Operation(
+            method = "POST",
+            summary = "Создать новое туристическое агентство",
+            description = "Создает новое туристическое агентство"
+    )
+    public ResponseEntity<String> addTraveler(
             @RequestBody Traveler traveler) {
-        try {
-            Traveler newTraveler = travelerService.addTraveler(traveler);
-            return ResponseEntity.status(201).body(newTraveler);
-        } catch (EntityNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        }
+        travelerService.addTraveler(traveler);
+        LOGGER.info("Created new Travel Agency");
+        return ResponseEntity.status(HttpStatus.CREATED).body("Successfully created a new agency");
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Traveler> updateTraveler(
+    @Operation(
+            method = "PUT",
+            summary = "Обновить туристическое агентство",
+            description = "Обновляет существующее туристическое агентство"
+    )
+    public ResponseEntity<String> updateTraveler(
             @PathVariable Long id,
             @RequestBody Traveler updatedTraveler) {
-            Traveler existingTraveler = travelerService.updateTraveler(id, updatedTraveler);
-            return ResponseEntity.ok(existingTraveler);
-
+        try {
+            travelerService.updateTraveler(id, updatedTraveler);
+            return ResponseEntity.ok("Successfully updated traveller with ID: " + id);
+        } catch (EntityNotFoundException e) {
+            LOGGER.error("Error: {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteTraveler(@PathVariable Long id) {
+    @Operation(
+            method = "DELETE",
+            summary = "Удалить туристическое агентство",
+            description = "Удаляет туристическое агентство по ID"
+    )
+    public ResponseEntity<String> deleteTraveler(@PathVariable Long id) {
         try {
             travelerService.deleteTraveler(id);
-            return ResponseEntity.noContent().build();
+            LOGGER.info("Deleted Travel Agency by ID: {}", id);
+            return ResponseEntity.ok("Successfully deleted agency with ID: " + id);
         } catch (EntityNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+            LOGGER.error("Error: {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         }
     }
 }
